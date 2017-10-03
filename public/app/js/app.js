d906e6e02ff0a197e83634fa491619ab849b9bb3
NguyenQuan <ngquancit@gmail.com>
2017-10-03 15:34:15 +0700

    delete menu and nav game

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -2,15 +2,9 @@ var app = angular.module("AutointegrityApp",['ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
 
-        .when('/carpart', {
+        .when('/', {
             templateUrl: 'CarPart/carpart.html',
             controller: 'CarPartController'
 
         })
-        .when('/mergepart', {
-            templateUrl: 'MergePart/merge-part.html',
-            controller: 'MergePartController'
-
-        })
-
 }]);
